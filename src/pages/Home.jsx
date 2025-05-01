b59ff2af09be3f29b9d9caf1de83a82cd63fd932
Vicky00711 <sahoovicky200127@gmail.com>
2025-05-02 00:42:03 +0100

    class to className

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -57,21 +57,21 @@ function Home() {
     };       
   return (
     <div>
-    <div class="bg-gray-50">
-      <div class="min-h-screen flex flex-col items-center justify-center py-6 px-4">
-        <div class="max-w-md w-full">
+    <div className="bg-gray-50">
+      <div className="min-h-screen flex flex-col items-center justify-center py-6 px-4">
+        <div className="max-w-md w-full">
           <a href="javascript:void(0)"><img
-            src="https://static.vecteezy.com/system/resources/previews/020/101/791/non_2x/admin-login-blue-gradient-concept-icon-administrator-account-learning-management-system-access-abstract-idea-thin-line-illustration-isolated-outline-drawing-vector.jpg" alt="logo" class="w-40 mb-8 mx-auto block" />
+            src="https://static.vecteezy.com/system/resources/previews/020/101/791/non_2x/admin-login-blue-gradient-concept-icon-administrator-account-learning-management-system-access-abstract-idea-thin-line-illustration-isolated-outline-drawing-vector.jpg" alt="logo" className="w-40 mb-8 mx-auto block" />
           </a>
 
-          <div class="p-8 rounded-2xl bg-white shadow">
-            <h2 class="text-slate-900 text-center text-3xl font-semibold">Sign in</h2>
-            <form class="mt-12 space-y-6">
+          <div className="p-8 rounded-2xl bg-white shadow">
+            <h2 className="text-slate-900 text-center text-3xl font-semibold">Sign in</h2>
+            <form className="mt-12 space-y-6">
               <div>
-                <label class="text-slate-800 text-sm font-medium mb-2 block">Email</label>
-                <div class="relative flex items-center">
-                  <input name="email" placeholder='Email Address' type='email' value={email} onChange={(e)=> setEmail(e.target.value)} required class="w-full text-slate-800 text-sm border border-slate-300 px-4 py-3 rounded-md outline-blue-600"  />
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" class="w-4 h-4 absolute right-4" viewBox="0 0 24 24">
+                <label className="text-slate-800 text-sm font-medium mb-2 block">Email</label>
+                <div className="relative flex items-center">
+                  <input name="email" placeholder='Email Address' type='email' value={email} onChange={(e)=> setEmail(e.target.value)} required className="w-full text-slate-800 text-sm border border-slate-300 px-4 py-3 rounded-md outline-blue-600"  />
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" className="w-4 h-4 absolute right-4" viewBox="0 0 24 24">
                     <circle cx="10" cy="7" r="6" data-original="#000000"></circle>
                     <path d="M14 15H6a5 5 0 0 0-5 5 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 5 5 0 0 0-5-5zm8-4h-2.59l.3-.29a1 1 0 0 0-1.42-1.42l-2 2a1 1 0 0 0 0 1.42l2 2a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42l-.3-.29H22a1 1 0 0 0 0-2z" data-original="#000000"></path>
                   </svg>
@@ -79,21 +79,21 @@ function Home() {
               </div>
 
               <div>
-                <label class="text-slate-800 text-sm font-medium mb-2 block">Password</label>
-                <div class="relative flex items-center">
-                  <input name="password" type="password" id='password' value={password} onChange={(e)=>setPassoword(e.target.value)} required class="w-full text-slate-800 text-sm border border-slate-300 px-4 py-3 rounded-md outline-blue-600" placeholder="Enter password" />
+                <label className="text-slate-800 text-sm font-medium mb-2 block">Password</label>
+                <div className="relative flex items-center">
+                  <input name="password" type="password" id='password' value={password} onChange={(e)=>setPassoword(e.target.value)} required className="w-full text-slate-800 text-sm border border-slate-300 px-4 py-3 rounded-md outline-blue-600" placeholder="Enter password" />
                   
                 </div>
               </div>
 
               
 
-              <div class="!mt-12">
-                <button type="button" onClick={handleSubmit} class="w-full py-2 px-4 text-[15px] font-medium tracking-wide rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none">
+              <div className="!mt-12">
+                <button type="button" onClick={handleSubmit} className="w-full py-2 px-4 text-[15px] font-medium tracking-wide rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none">
                   Sign in
                 </button>
               </div>
-              <p class="text-slate-800 text-sm !mt-6 text-center">Don't have an account? <a href="javascript:void(0);" class="text-blue-600 hover:underline ml-1 whitespace-nowrap font-semibold">Register here</a></p>
+              <p className="text-slate-800 text-sm !mt-6 text-center">Don't have an account? <a href="javascript:void(0);" className="text-blue-600 hover:underline ml-1 whitespace-nowrap font-semibold">Register here</a></p>
             </form>
           </div>
         </div>
